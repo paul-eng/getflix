aa9a16dc5b117ad663d93f2785a582dd640bff50
Paul <pje224@nyu.edu>
2021-05-25 03:30:06 -0400

    test plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,31 @@ const path = require('path');
 
 var webpack = require("webpack");
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
+// var plugins = []; // if using any plugins for both dev and production
+// var devPlugins = []; // if using any plugins for development
 
-var prodPlugins = [
+// var prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
+
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+
+plugins = [
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
-
+]
 
 module.exports = {
   context: __dirname,
