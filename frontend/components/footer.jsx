cc5c861c7d54cbd08d4983ea084cd9bd20db74d2
Paul <pje224@nyu.edu>
2021-05-27 01:11:21 -0400

    Update footer links

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -36,8 +36,8 @@ class Footer extends React.Component {
           <ul>
             <li><a href='https://www.linkedin.com/in/pauljoeng/' target='_blank'>LinkedIn</a></li>
             <li><a href='https://github.com/paul-eng/' target='_blank'>GitHub</a></li>
-            <li><a href='http://paul-eng.com/' target='_blank'>Portfolio</a></li>
-            <li><a href='http://paul-eng.com/assets/resume.pdf' target='_blank'>Resume</a></li>
+            <li><a href='https://paul-eng.github.io/' target='_blank'>Portfolio</a></li>
+            <li><a href='https://paul-eng.github.io/assets/resume.pdf' target='_blank'>Resume</a></li>
             <li><a href='https://angel.co/paul-eng' target='_blank'>AngelList</a></li>
             <li><a href='mailto:paul.jo.eng@gmail.com'>Contact</a></li>
           </ul>
@@ -45,9 +45,9 @@ class Footer extends React.Component {
             <a href='https://www.linkedin.com/in/pauljoeng/' target='_blank'><i className="fa fa-linkedin"></i></a>
             <a href='https://angel.co/paul-eng' target='_blank'><i className="fa fa-angellist"></i></a>
             <a href='https://github.com/paul-eng/' target='_blank'><i className="fa fa-github"></i></a>
-            <a href='http://paul-eng.com/' target='_blank'><i className="fa fa-drivers-license"></i></a>
+            <a href='https://paul-eng.github.io/' target='_blank'><i className="fa fa-drivers-license"></i></a>
             <a href='mailto:paul.jo.eng@gmail.com'><i className="fa fa-envelope"></i></a>
-            <a href='http://paul-eng.com/assets/resume.pdf' target='_blank'><i className="fa fa-file"></i></a>
+            <a href='https://paul-eng.github.io/assets/resume.pdf' target='_blank'><i className="fa fa-file"></i></a>
           </div>
           <p>© 1989-2017 Paul Eng, Inc.</p>
         </main>
